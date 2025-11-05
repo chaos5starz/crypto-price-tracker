627ce5f0cf7f554b6ce67733d3be466356b0dd6c
wessam mohamed <chaos5starz@gmail.com>
2025-11-05 20:41:20 +0200

    added error handling for incorrect coin name and internet issues and api issues

--- main.py
+++ main.py
@@ -6,26 +6,41 @@ load_dotenv()
 
 api_key = os.getenv("API_KEY")
 
-print("provide a coin name")
-coin = input()
+print("provide a coin name, type exit to exit")
 
-url = "https://api.coingecko.com/api/v3/coins/markets"
+while True :
 
-params = {
-    "vs_currency": "usd",   
-    "ids": coin             
-}
+    coin = input().lower()
 
-headers = {"x-cg-demo-api-key": api_key}
+    if coin == "exit" :
+        print("bye")
+        break
 
-response = requests.get(url, headers=headers , params=params)
-
-
-data = response.json()
+    url = "https://api.coingecko.com/api/v3/coins/markets" 
+    params = { "vs_currency": "usd", "ids": coin } 
+    headers = {"x-cg-demo-api-key": api_key} 
 
 
-for coin_dict in data:
-    print(f"the current price of {coin} is {coin_dict['current_price']} last updated in {coin_dict["last_updated"]}")
+    try:
+        response = requests.get(url, headers = headers , params=params , timeout=5)
+        response.raise_for_status()  # catch HTTP errors
+        data = response.json()
+    except requests.exceptions.Timeout:
+        print("Bro your internet is sleeping. Try again.")
+        continue
+    except Exception:
+        print("API messed up. Try later.")
+        continue
+
+
+    if data != [] :
+        print(f"the current price of {coin} is ${round(data[0]['current_price'], 2)} and it was last updated on {data[0]['last_updated']}")
+    else :
+        print("invalid coin")
+        continue
+
+    
+
 
 
 
