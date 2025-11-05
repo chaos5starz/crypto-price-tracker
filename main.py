7771df593668d2ee629f296714cba3c0442bd0f7
wessam mohamed <chaos5starz@gmail.com>
2025-11-05 19:26:43 +0200

    added headers and parameters and last update date

--- main.py
+++ main.py
@@ -7,18 +7,28 @@ load_dotenv()
 api_key = os.getenv("API_KEY")
 
 print("provide a coin name")
-
 coin = input()
 
+url = "https://api.coingecko.com/api/v3/coins/markets"
 
-url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin}&vs_currencies=usd&x_cg_api_key={api_key}"
+params = {
+    "vs_currency": "usd",   
+    "ids": coin             
+}
 
+headers = {"x-cg-demo-api-key": api_key}
 
-response = requests.get(url)
+response = requests.get(url, headers=headers , params=params)
 
 
 data = response.json()
 
-print (data[coin])
+
+for coin_dict in data:
+    print(f"the current price of {coin} is {coin_dict['current_price']} last updated in {coin_dict["last_updated"]}")
+
+
+
+
 
 
